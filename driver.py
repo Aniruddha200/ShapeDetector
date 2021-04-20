234239311cb0cf788947ec41621a1ce88f94ce66
Aniruddha Baidya <aniruddha200baidya@gmail.com>
2021-04-20 19:24:17 +0530

    Alpha 1 Commit.

--- driver.py
+++ driver.py
@@ -1,18 +1,20 @@
-from shapeDetector import ShapeDetector
+import cv2
 # import argparse
 import imutils
-import cv2
+
+from shapeDetector import ShapeDetector
 
 
 class Driver:
     def runDriver(self):
-
         # ap = argparse.ArgumentParser()
         # ap.add_argument("-i", "--image", required=True,
         #     help="path to the input image")
         # args = vars(ap.parse_args())
         # image = cv2.imread(args["image"])
-        image = cv2.imread("shapes2.jpg")
+        image = cv2.imread("shapes.png")
+        image = cv2.bitwise_not(image)
+
         resized = imutils.resize(image, width=300)
         ratio = image.shape[0] / float(resized.shape[0])
         # convert the resized image to grayscale, blur it slightly,
@@ -20,6 +22,7 @@ class Driver:
         gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
         # find contours in the thresholded image and initialize the
         # shape detector
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
@@ -38,8 +41,11 @@ class Driver:
             c = c.astype("float")
             c *= ratio
             c = c.astype("int")
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+
+            cv2.drawContours(image, [c], -1, (0, 0, 255), 2)
             cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        (255, 255, 255), 2)
+                        (128, 128, 128), 2)
             # show the output image
             cv2.imshow("Image", image)
+            cv2.waitKey(2000)
+        cv2.destroyAllWindows()
